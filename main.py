ff6d0916c8b821ebd3db6d839b7db4eb9469b5cb
Prashant9683 <amenu4elc21048@am.students.amrita.edu>
2023-04-03 22:31:31 +0530

    Added some commands

--- main.py
+++ main.py
@@ -13,7 +13,14 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    if message.content.startswith("!hello"):
+        await message.channel.send("Namah Shivaya! I am your new helper...")
+    if message.content.startswith("!help"):
+        await message.channel.send("You can use the following commands to interact with me: \n"
+                                   "$hello - I will greet you. \n"
+                                   "$help - I will tell you what I can do for you. \n"
+                                   "$about - I will tell you about myself. \n")
+    if message.content.startswith("!about"):
+        await message.channel.send("I am a Discord Bot. I am still learning. So, please be patient with me. I will be able to help you soon. Thank you for your patience!")
 
 client.run(os.environ['TOKEN'])
